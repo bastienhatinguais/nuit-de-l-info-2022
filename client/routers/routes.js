43f936c30f7986b10f2a51cf7487b119e359cd80
Sylvain Place <sylvain.place@etud.univ-jfc.fr>
2022-12-01 23:48:48 +0100

    Add Quizz.js

--- client/routers/routes.js
+++ client/routers/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 
 import Home from '../components/Home';
+import Quizz from '../components/Quizz/Quizz';
 
 const router = createBrowserRouter([
  
